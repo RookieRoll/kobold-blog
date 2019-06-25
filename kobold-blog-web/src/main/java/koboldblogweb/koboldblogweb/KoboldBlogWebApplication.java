7562fefe169e81dadb8075a3046a66a24de25d7a
RookieRoll <2807645688@qq.com>
2019-06-25 21:45:46 +0800

    添加缓存redis，待测试

--- kobold-blog-web/src/main/java/koboldblogweb/koboldblogweb/KoboldBlogWebApplication.java
+++ kobold-blog-web/src/main/java/koboldblogweb/koboldblogweb/KoboldBlogWebApplication.java
@@ -3,12 +3,14 @@ package koboldblogweb.koboldblogweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RefreshScope
+@EnableCaching
 public class KoboldBlogWebApplication {
 
 	public static void main(String[] args) {
---
+++ kobold-blog-web/src/main/java/koboldblogweb/koboldblogweb/config/RedisCacheConfig.java
@@ -0,0 +1,21 @@
+package koboldblogweb.koboldblogweb.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
+
+@Configurable
+public class RedisCacheConfig {
+    @Bean
+    public RedisCacheManager cacheManager(RedisTemplate redisTemplate) {
+        //全局redis缓存过期时间
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(1));
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisTemplate.getConnectionFactory());
+        return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+    }
+}
